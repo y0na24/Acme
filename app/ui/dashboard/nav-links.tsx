d8eb638e5dce4900c8c30415f05ae4532fa73761
Матвей Клёнов <y0na24@mail.ru>
2023-11-24 11:05:01 +0300

    fix

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import {
   UserGroupIcon,
@@ -8,7 +8,6 @@ import {
 import Link from 'next/link';
 import clsx from 'clsx';
 import { usePathname } from 'next/navigation';
-
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -23,8 +22,7 @@ const links = [
 ];
 
 export default function NavLinks() {
-  const pathname = usePathname()
-
+  const pathname = usePathname();
 
   return (
     <>
@@ -34,10 +32,11 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className={clsx("flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
-            {
-              'bg-sky-100 text-blue-600': pathname === link.href,
-            }
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
             )}
           >
             <LinkIcon className="w-6" />
