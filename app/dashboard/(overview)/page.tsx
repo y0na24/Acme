455ae3b08e44302edcf556da89db456d6994936a
Матвей Клёнов <y0na24@mail.ru>
2023-11-25 12:09:46 +0300

    add pagination

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -10,7 +10,6 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 
 export default async function Page() {
- 
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
